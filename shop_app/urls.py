7e21d1532e836edc7cb4537c44e9f8774b2a7538
jaehyun An <jaehyuna11@gmail.com>
2021-01-13 00:21:42 -0800

    navbar styles added

--- shop_app/urls.py
+++ shop_app/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="store"),
+    path('', views.store, name="store"),
     path('cart/', views.cart, name="cart"),
+    path('buy_it_now/', views.buy_it_now, name="buy_it_now"),
+    path('user_login/', views.user_login, name="user_login"),
+    path('user_registration/', views.user_registration, name="user_registration"),
 ]
--- shop_app/views.py
+++ shop_app/views.py
@@ -4,9 +4,27 @@ from .models import *
 # Create your views here.
 
 
-def index(request):
-    return render(request, 'shop_main.html')
+def store(request):
+    # create context to pass some data
+    context = {}
+    return render(request, 'shop_main.html', context)
+
+
+def user_login(request):
+    context = {}
+    return render(request, 'user_login.html', context)
+
+
+def user_registration(request):
+    context = {}
+    return render(request, 'user_registration.html', context)
 
 
 def cart(request):
-    return render(request, 'shop_cart.html')
+    context = {}
+    return render(request, 'shop_cart.html', context)
+
+
+def buy_it_now(request):
+    context = {}
+    return render(request, 'buy_it_now.html', context)
